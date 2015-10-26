fee17b74cbcc2967e38ea4c7e0977a7a800bb918
kaitokidi <genis.bayona@est.fib.upc.edu>
2015-10-26 09:18:53 +0100

    Update utils.hpp
    <3

--- utils.hpp
+++ utils.hpp
@@ -2,23 +2,27 @@
 #define UTILS_HPP
 
 //SFML (Simple Fast Media Library)
+#include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 
 //STL (Standard Toolkit Library)
-#include <cmath>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <cstring>
-#include <vector>
+#include <map>
 #include <list>
+#include <cmath>
 #include <stack>
 #include <queue>
-#include <map>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <functional>
+
+const float TO_DEGREES = (1 / (float ) M_PI) * 180;
+const float TO_RADIANS = (1 / 180.0f) * (float ) M_PI;
 
 // If you want to declare global variables you can do it here:
 extern int number; // You must to declare this avariable again on utils.cpp
