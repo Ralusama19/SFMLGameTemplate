3f8cb2aee5254611ac397b5ea3f411306f160457
Ralusama19 <ralucris.v@gmail.com>
2015-10-30 18:18:11 +0100

    finally fixed the typo, it was hard

--- utils.hpp
+++ utils.hpp
@@ -25,6 +25,6 @@ const float TO_DEGREES = (1 / (float ) M_PI) * 180;
 const float TO_RADIANS = (1 / 180.0f) * (float ) M_PI;
 
 // If you want to declare global variables you can do it here:
-extern int number; // You must declare this avariable again on utils.cpp
+extern int number; // You must declare this variable again on utils.cpp
 
 #endif // UTILS_HPP
