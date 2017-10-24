a567fb3fac92ed5833c3cd6e4c111861a4e15a68
Ralusama19 <ralucris.v@gmail.com>
2017-10-24 22:53:42 +0200

    comment resources

--- Resources.cpp
+++ Resources.cpp
@@ -1,7 +1,7 @@
 #include "Resources.hpp"
 
 // Explanation of this class:
-// This class allow you to transform this:
+// This class allows you to transform this:
 //
 // sf::Texture texture;
 // texture.loadFromFile("PATH");
@@ -19,8 +19,8 @@ sf::Font        Resources::font;
 sf::SoundBuffer Resources::sound;
 sf::Music       Resources::music;
 void Resources::load() {
-    texture.loadFromFile("resources/test1.png");
-    font.loadFromFile("resources/font.ttf");
-    sound.loadFromFile("resources/sound.wav"); //sound must be a short audio
-    music.openFromFile("resources/music.ogg"); //Im not sure if this a good way to do that :/
+    //texture.loadFromFile("resources/test1.png");
+    //font.loadFromFile("resources/font.ttf");
+    //sound.loadFromFile("resources/sound.wav"); //sound must be a short audio
+    //music.openFromFile("resources/music.ogg"); //Im not sure if this a good way to do that :/
 }
